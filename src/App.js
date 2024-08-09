cb930263497607bb09b2d9bada72b960f6563655
GovarthananD <govarthananduraisamy@gmail.com>
2024-08-09 13:09:43 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import { GoogleMap, LoadScript, Marker, Polyline } from '@react-google-maps/api';
+import io from 'socket.io-client';
 import './App.css';
 
+const socket = io.connect('http://localhost:6000');
+
+const containerStyle = {
+  width: '100%',
+  height: '400px'
+};
+
+const center = {
+  lat: 37.7749,
+  lng: -122.4194
+};
+
 function App() {
+  const [position, setPosition] = useState(center);
+  const [path, setPath] = useState([center]);
+
+  useEffect(() => {
+    socket.on('vehiclePosition', (newPosition) => {
+      setPosition(newPosition);
+      setPath((prevPath) => [...prevPath, newPosition]);
+    });
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <LoadScript googleMapsApiKey="YOUR_GOOGLE_MAPS_API_KEY">
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={position}
+        zoom={15}
+      >
+        <Marker position={position} />
+        <Polyline path={path} options={{ strokeColor: '#FF0000' }} />
+      </GoogleMap>
+    </LoadScript>
     </div>
   );
 }
